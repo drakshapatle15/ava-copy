3120de2f3517a510d7855f0ef10618025ad18f6b
drakshapatle15 <drakshakarmaleen@gmail.com>
2024-02-06 16:14:16 +0530

    remove nmodule

--- auth/router.js
+++ auth/router.js
@@ -1,38 +1,7 @@
-// const express = require('express');
-// const router = express.Router();
-// const { ObjectId } = require('bson');
-// const {User} = require("./models");
-
-// const authenticateUser = require("./middlewares")
-
-// router.post("/apikey/create", authenticateUser, async (req, res)=>{
-//     res.send({"mess":"hi"})
-// })
-
-// // router.post("/init-persona", async (req, res)=>{
-// //     const personas = await fetch("https://apiava.websperotech.com/persona")
-// //     const data = personas.json()
-
-// // })
-
-// module.exports = router
-
 const express = require("express");
 const router = express.Router();
 const { ObjectId } = require("bson");
 const { User } = require("./models");
-
-// const authenticateUser = require("./middlewares");
-
-// router.post("/apikey/create", authenticateUser, async (req, res) => {
-//   res.send({ mess: "hi" });
-// });
-
-// router.post("/init-persona", async (req, res)=>{
-//     const personas = await fetch("https://apiava.websperotech.com/persona")
-//     const data = personas.json()
-
-// })
 
 module.exports = router;
 
